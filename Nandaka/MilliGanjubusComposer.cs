9f8bbaf267b434f3162a3265e99d7ab827e94612
Vladimir Sidorenko <sidorenko.vl.al@gmail.com>
2019-08-05 01:44:15 +0300

    A lot of problems with composer

--- Nandaka/MilliGanjubusComposer.cs
+++ Nandaka/MilliGanjubusComposer.cs
@@ -1,22 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Nandaka
 {
-    public sealed class MilliGanjubusComposer : MilliGanjubusProtocolInfo, IComposer<byte[]>
+    public class MilliGanjubusComposer : IComposer<byte[]>
     {
+        // todo: how to make protocolInfo fields changeable? 
+        // Interface with getter-methods?
         readonly MilliGanjubusProtocolInfo _protocolInfo;
 
-        public MilliGanjubusComposer()
+        public MilliGanjubusComposer(MilliGanjubusProtocolInfo protocolInfo)
         {
-
+            _protocolInfo = protocolInfo;
         }
 
         public byte[] Compose(IProtocolMessage message)
         {
-            
-            //todo: who should resolve problem with oversized messages?
+            var data = GetDataBytes(message);
+            var packet = new byte[MilliGanjubusProtocolInfo.MinPacketLength + data.Length];
+            if (packet.Length > MilliGanjubusProtocolInfo.MaxPacketLength)
+            {
+                // todo: create a custom exception.
+                throw new ArgumentOutOfRangeException();
+            }
+            // todo: resolve problem with MilliGanjubusProcolInfoEveryFieldNamingStyle.
+            packet[MilliGanjubusProtocolInfo.StartByteOffset] = MilliGanjubusProtocolInfo.StartByte;
+            // todo: Add device address to IProtocolMessage interface?
+            packet[MilliGanjubusProtocolInfo.AddressOffset] = 0;
+            packet[MilliGanjubusProtocolInfo.SizeOffset] = (byte)packet.Length;
+            packet[MilliGanjubusProtocolInfo.HeaderCheckSumOffset] = 
+                Checksum.CRC8(packet.Take(MilliGanjubusProtocolInfo.HeaderCheckSumOffset).ToArray());
+            Array.Copy(data, 0, packet, MilliGanjubusProtocolInfo.DataOffset, data.Length);
+            packet[packet.Length - 1] = 
+                Checksum.CRC8(packet.Take(packet.Length - MilliGanjubusProtocolInfo.PacketCheckSumSize).ToArray());
+            return packet;
+        }
+
+        private byte[] GetDataBytes(IProtocolMessage message)
+        {
+            // Here I have to decide whether it's a series or a group. 
+            // Session does the same to calculate packet length.
             throw new NotImplementedException();
         }
     }
--- Nandaka/MilliGanjubusProtocolInfo.cs
+++ Nandaka/MilliGanjubusProtocolInfo.cs
@@ -4,38 +4,33 @@ using System.Text;
 
 namespace Nandaka
 {
-    public class MilliGanjubusProtocolInfo : GanjubusBaseProtocolInfo
+    public class MilliGanjubusProtocolInfo
     {
         // MinPacketLength = Header + Address + Length + CRC8-head + CRC-8 tail.
-        public readonly int MinPacketLength = 5;
+        public const int MinPacketLength = 5;
         // 15 байт максимальный блок данных для CRC8 + само CRC
-        public readonly int MaxPacketLength = 15 + 1;
+        public const int MaxPacketLength = 15 + 1;
         // Минус Gbyte.
-        public readonly int MaxDataLength = 10;
-        public readonly int StartByteOffset = 0;
-        public readonly int AddressOffset = 1;
-        public readonly int SizeOffset = 2;
-        public readonly int HeaderCheckSumOffset = 3;
-        public readonly int DataOffset = 4;
-        public readonly int HeaderCheckSumSize = sizeof(byte);
-        public readonly int PacketCheckSumSize = sizeof(byte);
+        public const int MaxDataLength = MaxPacketLength - MinPacketLength - 1;
+        public const int StartByteOffset = 0;
+        public const int AddressOffset = 1;
+        public const int SizeOffset = 2;
+        public const int HeaderCheckSumOffset = 3;
+        public const int DataOffset = 4;
+        public const int HeaderCheckSumSize = sizeof(byte);
+        public const int PacketCheckSumSize = sizeof(byte);
 
-        public readonly byte GAcknowledge = 0xA;
-        public readonly byte GRequest = 0x0;
-        public readonly byte GError = 0xE;
+        public const byte GAcknowledge = 0xA;
+        public const byte GRequest = 0x0;
+        public const byte GError = 0xE;
 
-        public readonly byte FWriteSingle = 0x1;
-        public readonly byte FWriteRange = 0x2;
-        public readonly byte FWriteSeries = 0x3;
-        public readonly byte FReadSingle = 0x4;
-        public readonly byte FReadSeries = 0x6;
-        public readonly byte FReadRange = 0x5;
-        public readonly int MinimumRangeRegisterCount = 3;
-        public readonly byte StartByte = 0xBB;
-
-        public int GetMaxDataLength()
-        {
-            return MaxDataLength;
-        }
+        public const byte FWriteSingle = 0x1;
+        public const byte FWriteRange = 0x2;
+        public const byte FWriteSeries = 0x3;
+        public const byte FReadSingle = 0x4;
+        public const byte FReadSeries = 0x6;
+        public const byte FReadRange = 0x5;
+        public const int MinimumRangeRegisterCount = 3;
+        public const byte StartByte = 0xBB;
     }
 }
