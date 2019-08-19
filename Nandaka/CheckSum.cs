42a1c16b6c0cd48bb966d702fff1b306314dd51f
Vladimir Sidorenko <sidorenko.vl.al@gmail.com>
2019-08-19 14:16:28 +0300

    Re-added checksum class from local repository

---
+++ Nandaka/CheckSum.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Nandaka
+{
+    public class CheckSum
+    {
+        public static byte CRC8(byte[] pcBlock)
+        {
+            return CRC8(pcBlock, pcBlock.Length);
+        }
+
+        private static byte CRC8(byte[] pcBlock, int len)
+        {
+            byte crc = 0xFF;
+
+            for (int k = 0; k < len; k++)
+            {
+                crc ^= pcBlock[k];
+
+                for (int i = 0; i < 8; i++)
+
+                    crc = (byte)(((crc & 0x80) != 0) ? (crc << 1) ^ 0x31 : crc << 1);
+            }
+            return crc;
+        }
+    }
+}
