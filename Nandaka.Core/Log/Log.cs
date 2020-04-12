7d0be3d0459a6d1f36f0092b45d20fcc8f15ddbf
Vladimir Sidorenko <sidorenko.vl.al@gmail.com>
2020-04-13 01:38:25 +0300

    Logger implemented (very bad)

--- Nandaka.Core/Log/Log.cs
+++ Nandaka.Core/Log/Log.cs
@@ -1,21 +1,38 @@
-﻿using System.Diagnostics.CodeAnalysis;
+﻿using System;
+using System.Collections.Concurrent;
+using System.IO;
 using System.Threading;
+using Nandaka.Core.Helpers;
 
 namespace Nandaka.Core
 {
-    internal class Log : ILog
+    internal class Log : ILog, IDisposable
     {
-        private static volatile ILog _instance;
+        // todo: define options in separate entity
+        private const string LogFileName = "Nandaka.log";
+        private static readonly string s_appDataFolderPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
+            "Nandaka");
+
+        private static volatile ILog s_instance;
         private static readonly object s_syncRoot = new object();
+        private bool _isDisposed;
+
+        private readonly StreamWriter _writer;
+        private readonly BlockingCollection<string> _buffer;
+        private readonly Thread _thread;
 
         private Log()
         {
-            // todo: implement this.
+            string logFilePath = Path.Combine(s_appDataFolderPath, LogFileName);
+            _writer = new StreamWriter(logFilePath) { AutoFlush = true };
+            _buffer = new BlockingCollection<string>();
+            _thread = new Thread(Routine) { IsBackground = true };
+            _thread.Start();
         }
 
         private static Log Create()
         {
-            // todo: implement this.
+            Directory.CreateDirectory(s_appDataFolderPath);
             return new Log();
         }
         
@@ -23,24 +40,68 @@ namespace Nandaka.Core
         {
             get
             {
-                if (_instance != null)
-                    return _instance;
+                if (s_instance != null)
+                    return s_instance;
 
                 lock (s_syncRoot)
                 {
-                    if (_instance != null)
-                        return _instance;
+                    if (s_instance != null)
+                        return s_instance;
 
-                    _instance = Create();
+                    s_instance = Create();
                 }
 
-                return _instance;
+                return s_instance;
             }
         }
 
         public void AppendMessage(LogMessageType type, string message)
         {
-            // todo: implement this.
+            if (_isDisposed)
+                return;
+            
+            _buffer.Add($"[{DateTime.Now}][{type}]{message}");
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+
+
+        private void Routine()
+        {
+            while (true)
+            {
+                _writer.WriteLine(_buffer.Take());
+            }
+        }
+
+        ~Log()
+        {
+            Dispose(false);
+        }
+        
+        private void Dispose(bool isDisposing)
+        {
+            if (_isDisposed)
+                return;
+
+            if (isDisposing)
+            {
+                Flush();
+                _writer.Dispose();
+                _buffer.Dispose();
+            }
+
+            _isDisposed = true;
+        }
+
+        private void Flush()
+        {
+            while (!_buffer.IsEmpty())
+                _writer.WriteLine(_buffer.Take());
+            _writer.Flush();
         }
     }
 }
