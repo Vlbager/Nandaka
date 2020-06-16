d6fd2e9085fc47763dcff8dfce944281d314faff
Vladimir Sidorenko <sidorenko.vl.al@gmail.com>
2020-06-17 00:11:40 +0300

    Example updated with new user friendly design

--- Example/MasterExample/TestDevice.cs
+++ Example/MasterExample/TestDevice.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using Nandaka.Core.Attributes;
 using Nandaka.Core.Device;
 using Nandaka.Core.Session;
 using Nandaka.Core.Table;
@@ -7,79 +8,23 @@ namespace Example.MasterExample
 {
     public class TestDevice : NandakaDevice
     {
-        #region RO
-        private readonly IValuedRegister<int> _intRoRegister;
-        private readonly IValuedRegister<byte> _byteRoRegister;
-        private readonly IValuedRegister<short> _shortRoRegister;
-        private readonly IValuedRegister<long> _longRoRegister;
-        private readonly IValuedRegister<ulong> _ulongRoRegister;
+        public IRoRegister<int> IntRoRegister { get; }     = Int32RegisterGroup.CreateNew(0x00);
+        public IRoRegister<byte> ByteRoRegister { get; }   = UInt8RegisterGroup.CreateNew(0x04);   
+        public IRoRegister<short> ShortRoRegister { get; } = Int16RegisterGroup.CreateNew(0x05);    
+        public IRoRegister<long> LongRoRegister { get; }   = Int64RegisterGroup.CreateNew(0x07);  
+        public IRoRegister<ulong> UlongRoRegister { get; } = UInt64RegisterGroup.CreateNew(0x0F);
+        
+        public IRwRegister<int> IntRwRegister { get; }     = Int32RegisterGroup.CreateNew(0x80);
+        [RegisterUpdateInterval(2000)]
+        public IRwRegister<byte> ByteRwRegister { get; }   = UInt8RegisterGroup.CreateNew(0x84);
+        public IRwRegister<short> ShortRwRegister { get; } = Int16RegisterGroup.CreateNew(0x85);
+        public IRwRegister<long> LongRwRegister { get; }   = Int64RegisterGroup.CreateNew(0x87);
+        public IRwRegister<ulong> UlongRwRegister { get; } = UInt64RegisterGroup.CreateNew(0x8F);
 
-        public int RoInt => _intRoRegister.Value;
-        public byte RoByte => _byteRoRegister.Value;
-        public short RoShort => _shortRoRegister.Value;
-        public long RoLong => _longRoRegister.Value;
-        public ulong RoUlong => _ulongRoRegister.Value;
-        #endregion
-
-        #region RW
-        private readonly IValuedRegister<int> _intRwRegister;
-        private readonly IValuedRegister<byte> _byteRwRegister;
-        private readonly IValuedRegister<short> _shortRwRegister;
-        private readonly IValuedRegister<long> _longRwRegister;
-        private readonly IValuedRegister<ulong> _ulongRwRegister;
-
-        public int RwInt
-        {
-            get => _intRwRegister.Value;
-            set => SetRegisterValue(_intRwRegister, value);
-        }
-
-        public byte RwByte
-        {
-            get => _byteRwRegister.Value;
-            set => SetRegisterValue(_byteRwRegister, value);
-        }
-
-        public short RwShort
-        {
-            get => _shortRwRegister.Value;
-            set => SetRegisterValue(_shortRwRegister, value);
-        }
-
-        public long RwLong
-        {
-            get => _longRwRegister.Value;
-            set => SetRegisterValue(_longRwRegister, value);
-        }
-
-        public ulong RwUlong
-        {
-            get => _ulongRwRegister.Value;
-            set => SetRegisterValue(_ulongRwRegister, value);
-        }
-        #endregion
-
-        public override IReadOnlyCollection<IRegisterGroup> RegisterGroups { get; }
         public override string Name => nameof(TestDevice);
 
         private TestDevice(int address, IRegistersUpdatePolicy updatePolicy, DeviceState state) : base(address, updatePolicy, state)
-        {
-            var validator = new RegisterTableValidator();
-            
-            _intRoRegister   = validator.AddGroup(Int32RegisterGroup.CreateNew(0x00, RegisterType.Read));
-            _byteRoRegister  = validator.AddGroup(UInt8RegisterGroup.CreateNew(0x04, RegisterType.Read));
-            _shortRoRegister = validator.AddGroup(Int16RegisterGroup.CreateNew(0x05, RegisterType.Read));
-            _longRoRegister  = validator.AddGroup(Int64RegisterGroup.CreateNew(0x07, RegisterType.Read));
-            _ulongRoRegister = validator.AddGroup(UInt64RegisterGroup.CreateNew(0x0F, RegisterType.Read));
-            
-            _intRwRegister   = validator.AddGroup(Int32RegisterGroup.CreateNew(0x80, RegisterType.ReadWrite));
-            _byteRwRegister  = validator.AddGroup(UInt8RegisterGroup.CreateNew(0x84, RegisterType.ReadWrite));
-            _shortRwRegister = validator.AddGroup(Int16RegisterGroup.CreateNew(0x85, RegisterType.ReadWrite));
-            _longRwRegister  = validator.AddGroup(Int64RegisterGroup.CreateNew(0x87, RegisterType.ReadWrite));
-            _ulongRwRegister = validator.AddGroup(UInt64RegisterGroup.CreateNew(0x8F, RegisterType.ReadWrite));
-
-            RegisterGroups = validator.GetGroups();
-        }
+        { }
         
         public static TestDevice Create()
         {
--- Example/Program.cs
+++ Example/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.ComponentModel;
 using System.Diagnostics.CodeAnalysis;
 using System.Threading;
 using Example.MasterExample;
@@ -15,7 +14,6 @@ namespace Example
         public static void Main()
         {
             MasterExample();
-            //LocalMasterSlaveExample();
         }
 
         private static void MasterExample()
@@ -25,61 +23,24 @@ namespace Example
             var serialPortProvider = new SerialDataPortProvider("COM3", 57600);
             var protocol = MilliGanjubus.Create(serialPortProvider);
             
-            var manager = new TestMasterManager();
+            var manager = new MasterDeviceManager();
+            var device = TestDevice.Create();
+            
+            manager.AddSlaveDevice(device);
             manager.Start(protocol, new ForceUpdatePolicy(TimeSpan.FromMilliseconds(300), TimeSpan.FromMilliseconds(1)));
 
-            TestDevice device = manager.TestDevice;
-            
-            device.PropertyChanged += DeviceOnPropertyChanged;
-            
-            device.RwByte  = 3;
-            device.RwShort = 3;
-            device.RwInt   = 3;
-            device.RwLong  = 3;
-            device.RwUlong = 3;
+            device.ByteRoRegister.OnRegisterChanged += (sender, args) => Console.WriteLine("Byte ro register has been changed");
+
+            device.ByteRwRegister.Value  = 1;
+            device.ShortRwRegister.Value = 2;
+            device.IntRwRegister.Value   = 3;
+            device.LongRwRegister.Value  = 4;
+            device.UlongRwRegister.Value = 5;
 
             while (true)
             {
-                
-                
-                //Console.WriteLine(device.RoByte);
-                
                 Thread.Sleep(1000);
             }
-        }
-
-        private static void DeviceOnPropertyChanged(object sender, PropertyChangedEventArgs e)
-        {
-            if (e.PropertyName == nameof(TestDevice.RoLong))
-                Console.WriteLine($"RoLong property has been changed by slave device");
-        }
-
-        private static void LocalMasterSlaveExample()
-        {
-            Console.WriteLine("LocalMasterSlaveExample Started");
-
-            var serialPortProvider = new SerialDataPortProvider("COM3");
-            
-            var slaveManager = ConcreteSlaveManager.Create();
-            slaveManager.Start(MilliGanjubus.Create(serialPortProvider));
-            
-            var masterManager = ConcreteMasterManager.Create();
-            masterManager.Start(MilliGanjubus.Create(serialPortProvider), new ForceUpdatePolicy(Int32.MaxValue));
-
-            while (true)
-            {
-                byte currentByteValue = slaveManager.ConcreteDevice.TestByte;
-                int currentIntValue = slaveManager.ConcreteDevice.TestInt;
-                int currentReadOnlyValue = masterManager.ConcreteDevice.TestShort;
-                Console.WriteLine($"current write-only byte value on slave device: {currentByteValue}");
-                Console.WriteLine($"current write-only int value on slave device: {currentIntValue}");
-                Console.WriteLine($"current read-only short value on master device: {currentReadOnlyValue}");
-                
-                masterManager.ConcreteDevice.TestByte++;
-                masterManager.ConcreteDevice.TestInt++;
-                slaveManager.ConcreteDevice.TestShort++;
-                Console.ReadLine();
-            }    
         }
     }
 }
